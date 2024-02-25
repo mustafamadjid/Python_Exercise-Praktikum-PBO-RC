1bc2d9efc55909cf591ad5167324f11d0d63e948
mustafamadjid <madjidmustafa@gmail.com>
2024-02-25 16:22:38 +0700

    adding PS-2 and me text

---
+++ week0-pbo-rc/PS-2.py
@@ -0,0 +1,13 @@
+n = int(input("Number of students : "))
+print()
+
+grade = {}
+
+for i in range(n):
+   name = input("Student's name : ")
+   score = int(input("Score : "))
+   print()
+   grade.update({name : score}) 
+print()
+
+print(grade)
